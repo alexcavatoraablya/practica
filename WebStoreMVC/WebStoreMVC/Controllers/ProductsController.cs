cb7f4cca3ccff9651129fba5b9a6bec73b8aeec2
CODER <CODER@rivne.itstep.org>
2026-06-12 11:46:28 +0300

    cart and list products

---
+++ WebStoreMVC/WebStoreMVC/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStoreMVC.Data;
+using WebStoreMVC.Mapper;
+
+namespace WebStoreMVC.Controllers
+{
+    public class ProductsController(MyContextShopMVC myContext,
+        ProductMapper productMapper) : Controller
+    {
+        public IActionResult Details(string slug)
+        {
+            Console.WriteLine("Slug = "+slug);
+            //читвє товари з БД і відображає його
+            var item = myContext.Products
+                .Include(x => x.Category)
+                .Include(x => x.ProductImages)
+                .SingleOrDefault(x => x.Slug == slug);
+            var model = productMapper
+                .ProductEntityToProductItemModel(item);
+            return View(model);
+        }
+    }
+}
